2bcef7b401830a9617938c5a941509aaf49dde96
Lokendrakushwah12 <Lokendrakushwah8051@gmail.com>
2023-08-08 21:12:28 +0530

    created back btn

--- 2. Color switcher/index.js
+++ 2. Color switcher/index.js
@@ -1,6 +1,8 @@
 const btn = document.querySelectorAll("button");
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1");
+const back = document.getElementsByClassName("btn-back");
+
 
 btn.forEach(function (e) {
     e.addEventListener("click", function (ev) {
@@ -8,22 +10,27 @@ btn.forEach(function (e) {
         if (ev.target.id == "color1-btn") {
             body.style.backgroundImage = "url(red.svg)";
             h1.style.backgroundImage = "var(--red)";
+            back[0].style.fill = "hsl(0, 100%, 50%)";
         }
         if (ev.target.id == "color2-btn") {
             body.style.backgroundImage = "url(yellow.svg)";
             h1.style.backgroundImage = "var(--yellow)";
+            back[0].style.fill = "hsl(52, 100%, 50%)";
         }
         if (ev.target.id == "color3-btn") {
             body.style.backgroundImage = "url(green.svg)";
             h1.style.backgroundImage = "var(--green)";
+            back[0].style.fill = "hsl(102, 92%, 38%)";
         }
         if (ev.target.id == "color4-btn") {
             body.style.backgroundImage = "url(bg.svg)";
             h1.style.backgroundImage = "var(--blue)";
+            back[0].style.fill = "hsl(206, 100%, 50%)";
         }
         if (ev.target.id == "color5-btn") {
             body.style.backgroundImage = "url(white.svg)";
             h1.style.backgroundImage = "var(--white)";
+            back[0].style.fill = "hsl(0, 0%, 59%)";
         }
     })
 })
